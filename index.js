208dd6e2016396814a79cdf66407a5ede238a9d4
johannadznd <johanna.dezarnaud@ynov.com>
2021-10-26 10:10:15 +0200

    Add the hash for API

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const path = require('path')
+const axios = require('axios');
 const express = require('express')
-const { default: axios } = require('axios')
 const app = express()
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public/'))
 app.use(express.json({ limit: '50MB' }));
-const API_KEY = 'caf18c107ef554950dde4ffa1ff1a8ae'
+const API_KEY = '6a8ad0913e3b302f2b2a90716ec18a09'
+const TS = '1'
+const HASH = 'c2566addb3e2479e52f3fa755b3f044c'
 app.use(express.json({ limit: '50MB' }));
-axios.defaults.baseURL = `https://gateway.marvel.com:443/v1/public`;
+axios.defaults.baseURL = `https://gateway.marvel.com/v1/public`;
 axios.defaults.params = { apikey: API_KEY }
+axios.defaults.params = { ts: TS }
+axios.defaults.params = { hash: HASH }
+
+
+console.log(axios)
+
 
 app.get('/', (req, res) => {
   res.render('home', { message: 'Welcome folks!' })
@@ -22,14 +30,16 @@ app.get('/heroes', async (req, res) => {
       console.log(error);
     });
 
-  [hereos] = await Promise.all(
+/*   [hereos] = await Promise.all(
     hereos.map(async hero => {
       const comics = await axios.get(`characters/${hero.id}/comics`)
       hero.comics = comics
     })
-  )
+  )  */
 
   res.send(hereos)
+  console.log(hereos)
+
 });
 
 const port = 3000
