c83a55a200d153b7a2d10ceb0a515ee019680d91
MatthiasChometon <matthias.chometon@ynov.com>
2021-10-26 10:41:14 +0100

    feat: remove stringify

--- index.js
+++ index.js
@@ -6,18 +6,17 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public/'))
-app.use(express.json({ limit: '50MB' }));
+app.use(express.json({ limit: '50MB' }))
 const API_KEY = '6a8ad0913e3b302f2b2a90716ec18a09'
 const TS = '1'
 const HASH = 'c2566addb3e2479e52f3fa755b3f044c'
-app.use(express.json({ limit: '50MB' }));
-axios.defaults.baseURL = `https://gateway.marvel.com/v1/public/`;
-const params = {
+app.use(express.json({ limit: '50MB' }))
+axios.defaults.baseURL = `https://gateway.marvel.com/v1/public/`
+axios.defaults.params = params = {
   apikey: API_KEY,
   ts: TS,
   hash: HASH
 }
-axios.defaults.params = params
 
 app.get('/', (req, res) => {
   res.render('home', { message: 'Welcome folks!' })
@@ -26,13 +25,11 @@ app.get('/', (req, res) => {
 app.get('/heroes', async (req, res) => {
   let hereos = await axios.get('characters')
     .catch(function (error) {
-      console.log(error);
-    });
+      res.json(error)
+    })
 
-  const stringifyHereos = JSON.stringify(hereos.data)
-
-  res.json(stringifyHereos)
-});
+  res.json(hereos.data)
+})
 
 const port = 3000
 app.listen(port, () => console.log(`Listening on ${port}`))
