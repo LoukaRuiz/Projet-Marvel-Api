f87e1501782a1e499c31d482a353362fbaff1076
MatthiasChometon <matthias.chometon@ynov.com>
2021-10-26 16:45:11 +0100

    feat: add getAll service

---
+++ src/services/api/getAll.js
@@ -0,0 +1,29 @@
+const axios = require('axios')
+
+exports.getAll = async (req, res, parentDataName, childsName) => {
+    const parentData = await axios.get(parentDataName)
+        .catch(function (error) {
+            res.json(error)
+        })
+
+    const [childs] = await Promise.all(
+        await childsName.map(async childName => await getDatas(parentData.data.data.results, parentDataName, childName))
+    )
+
+    for (let child = 0; child < childsName.length; child++) {
+        parentData.data.data.results[childsName[child]] = childs[child]
+    }
+
+    res.json(parentData.data)
+}
+
+async function getDatas(parentData, parentDataName, ressourceName) {
+    return await Promise.all(
+        parentData.map(async parent => {
+            console.log(`${parentDataName}/${parent.id}/${ressourceName}`)
+            const child = await axios.get(`${parentDataName}/${parent.id}/${ressourceName}`)
+            parent[ressourceName] = child.data?.data.results ?? []
+            return parent
+        })
+    )
+}
