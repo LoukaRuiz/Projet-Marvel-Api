39f4d0c31f939ea1bd3ee23e2a9974cb367cf417
MatthiasChometon <matthias.chometon@ynov.com>
2021-10-26 16:45:26 +0100

    feat: add getAll()

--- src/Controllers/heroes.js
+++ src/Controllers/heroes.js
@@ -1,28 +1,5 @@
-const axios = require('axios')
+const { getAll } = require('../services/api/getAll')
 
 exports.getAllHeroes = async (req, res) => {
-    const heroes = await axios.get('characters')
-        .catch(function (error) {
-            res.json(error)
-        })
-
-    const [stories, comics] = await Promise.all(
-        await getHeroesDatas(heroes.data.data.results, 'stories'),
-        await getHeroesDatas(heroes.data.data.results, 'comics')
-    )
-
-    heroes.comics = comics
-    heroes.stories = stories
-
-    res.json(heroes.data)
-}
-
-async function getHeroesDatas(heroes, ressourceName) {
-    return await Promise.all(
-        heroes.map(async hero => {
-            const stories = await axios.get(`characters/${hero.id}/${ressourceName}`)
-            hero[ressourceName] = stories.data?.data.results ?? []
-            return hero
-        })
-    )
+    await getAll(req, res, 'characters', ['stories', 'comics'])
 }
