606082a19f68a41a84703620b488ca795577a80d
johannadznd <johanna.dezarnaud@ynov.com>
2021-10-26 15:17:59 +0200

    Add comics

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const axios = require('axios')
 const app = express()
 
 const heroes = require('./Routes/heroes')
+const comics = require('./Routes/comics')
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public/'))
@@ -23,6 +24,8 @@ const params = {
 axios.defaults.params = params
 
 app.use('/heroes/', heroes)
+app.use('/comics/', comics)
+
 app.get('/', (req, res) => {
   res.render('home', { message: 'Welcome folks!' })
 })
