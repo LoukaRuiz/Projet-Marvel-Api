fbddae429fc4418b68a8f24f8815415f9229560e
MatthiasChometon <matthias.chometon@ynov.com>
2021-10-26 10:33:21 +0100

    fix: get hereos

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const axios = require('axios');
 const express = require('express')
 const axios = require('axios')
 const app = express()
@@ -12,17 +11,20 @@ const API_KEY = '6a8ad0913e3b302f2b2a90716ec18a09'
 const TS = '1'
 const HASH = 'c2566addb3e2479e52f3fa755b3f044c'
 app.use(express.json({ limit: '50MB' }));
-axios.defaults.baseURL = `https://gateway.marvel.com/v1/public`;
-axios.defaults.params = { apikey: API_KEY }
-axios.defaults.params = { ts: TS }
-axios.defaults.params = { hash: HASH }
+axios.defaults.baseURL = `https://gateway.marvel.com/v1/public/`;
+const params = {
+  apikey: API_KEY,
+  ts: TS,
+  hash: HASH
+}
+axios.defaults.params = params
 
 app.get('/', (req, res) => {
   res.render('home', { message: 'Welcome folks!' })
 })
 
 app.get('/heroes', async (req, res) => {
-  let hereos = await axios.get('hereos')
+  let hereos = await axios.get('characters')
     .catch(function (error) {
       console.log(error);
     });
