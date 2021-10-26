461333563731ac5a52dced40e9199b811f421fe5
MatthiasChometon <matthias.chometon@ynov.com>
2021-10-26 12:27:17 +0100

    feat: get comics and stories for heroes

--- index.js
+++ index.js
@@ -12,24 +12,43 @@ const TS = '1'
 const HASH = 'c2566addb3e2479e52f3fa755b3f044c'
 app.use(express.json({ limit: '50MB' }))
 axios.defaults.baseURL = `https://gateway.marvel.com/v1/public/`
-axios.defaults.params = params = {
+const params = {
   apikey: API_KEY,
   ts: TS,
   hash: HASH
 }
+axios.defaults.params = params
 
 app.get('/', (req, res) => {
   res.render('home', { message: 'Welcome folks!' })
 })
 
 app.get('/heroes', async (req, res) => {
-  let hereos = await axios.get('characters')
+  const heroes = await axios.get('characters')
     .catch(function (error) {
       res.json(error)
     })
 
-  res.json(hereos.data)
+  const [stories, comics] = await Promise.all(
+    await getHeroesDatas(heroes.data.data.results, 'stories'),
+    await getHeroesDatas(heroes.data.data.results, 'comics')
+  )
+
+  heroes.comics = comics
+  heroes.stories = stories
+
+  res.json(heroes.data)
 })
+
+async function getHeroesDatas(heroes, ressourceName) {
+  return await Promise.all(
+    heroes.map(async hero => {
+      const stories = await axios.get(`characters/${hero.id}/${ressourceName}`)
+      hero[ressourceName] = stories.data?.data.results ?? []
+      return hero
+    })
+  )
+}
 
 const port = 3000
 app.listen(port, () => console.log(`Listening on ${port}`))
