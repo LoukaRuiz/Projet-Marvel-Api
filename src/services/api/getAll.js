eef3936692e0776df57be4d91e1976a86e824c2b
MatthiasChometon <matthias.chometon@ynov.com>
2021-10-27 15:38:07 +0100

    feat: delete space

--- src/services/api/getAll.js
+++ src/services/api/getAll.js
@@ -20,7 +20,6 @@ exports.getAll = async (req, res, parentDataName, childsName) => {
 async function getDatas(parentData, parentDataName, ressourceName) {
     return await Promise.all(
         parentData.map(async parent => {
-
             const child = await axios.get(`${parentDataName}/${parent.id}/${ressourceName}`)
             parent[ressourceName] = child.data?.data.results ?? []
             return parent
