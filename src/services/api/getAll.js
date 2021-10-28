6f4fa840b785c1c91405af1ab5a44c9836548b8b
johannadznd <johanna.dezarnaud@ynov.com>
2021-10-28 17:12:09 +0200

    Fix: Generic child route

--- src/services/api/getAll.js
+++ src/services/api/getAll.js
@@ -1,17 +1,26 @@
 const axios = require('axios')
+const env = process.env
 
 exports.getAll = async (req, res, parentDataName, childsName) => {
     const parentData = await axios.get(parentDataName)
         .catch(function (error) {
             res.json(error)
         })
+        
+        var paths = env.PATHS.split(",");
 
+        childRoute = parentData
+
+        paths.forEach((element, index) => {
+            childRoute = childRoute[paths[index]]
+        });
+        
     const [childs] = await Promise.all(
-        childsName.map(async childName => await getDatas(parentData.data.data.results, parentDataName, childName))
+        await childsName.map(async childName => await getDatas(childRoute, parentDataName, childName))
     )
 
     for (let child = 0; child < childsName.length; child++) {
-        parentData.data.data.results[childsName[child]] = childs[child]
+        childRoute[childsName[child]] = childs[child]
     }
 
     res.json(parentData.data)
@@ -20,6 +29,7 @@ exports.getAll = async (req, res, parentDataName, childsName) => {
 async function getDatas(parentData, parentDataName, ressourceName) {
     return await Promise.all(
         parentData.map(async parent => {
+
             const child = await axios.get(`${parentDataName}/${parent.id}/${ressourceName}`)
             parent[ressourceName] = child.data?.data.results ?? []
             return parent
